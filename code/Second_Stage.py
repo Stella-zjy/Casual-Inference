bb08965344d3db91c5dae466c9df9095328a8d44
williamkunhan <71636278+williamkunhan@users.noreply.github.com>
2022-08-14 08:50:42 -0400

    delete 2 verision of stage2

--- code/Second_Stage.py
+++ code/Second_Stage.py
@@ -20,16 +20,16 @@ class MLP(nn.Module):
         self.output_fc = nn.Linear(10, output_dim)
 
     def forward(self, x):
-        # x = [batch size, height, width]
+ 
         batch_size = x.shape[0]
         x = x.view(batch_size, -1)
-        # x = [batch size, height * width]
+ 
         h_1 = F.relu(self.input_fc(x))
-        # h_1 = [batch size, 250]
+
         h_2 = F.relu(self.hidden_fc(h_1))
-        # h_2 = [batch size, 100]
+  
         h3 = self.output_fc(h_2)
-        # y_pred = [batch size, output dim]
+
         y_pred = F.sigmoid(h3)
         #y_pred = F.softmax(h3, dim=1)
         return y_pred
